fea944225188c54aff029087e89cf6aa0ff56dee
Robert Stine <rcorystine@gmail.com>
2020-12-31 16:36:35 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,16 @@
-# code here!
+class School
+  
+  def initialize(school_name)
+    @name = school_name
+  end
+  
+  def roster
+    roster = {}
+  end
+  
+  def add_student(student_name, grade)
+    
+    
+    roster[grade] << student_name
+  end
+end
