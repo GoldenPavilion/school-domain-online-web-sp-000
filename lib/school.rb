49108e502d086c922cc97a7219bc4b950e45ef24
Robert Stine <rcorystine@gmail.com>
2020-12-31 17:08:22 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,7 +10,9 @@ class School
   end
   
   def add_student(student_name, grade)
-    roster[grade] << student_name
-    roster[grade] = []
+    if roster[grade] == nil
+      roster[grade] << student_name
+      roster[grade] = []
+    end
   end
 end
